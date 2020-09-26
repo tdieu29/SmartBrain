9c90f16d3e67d1d89345131f31dca21d43ee6133
Trang Dieu <trangdieu.kp@gmail.com>
2020-09-26 15:45:46 -0400

    Add components to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const particlesParams = {
+  particles: {
+    number: {
+      density: {
+        enable: true,
+        value_area: 900
+      },
+      value: 90
+    }
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesParams} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition />*/}
       </div>
